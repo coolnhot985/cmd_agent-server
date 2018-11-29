548046bd1c3c78a07ad112f6113c0538b66a4476
root <root@ewbf.ml>
2018-11-29 18:44:26 +0900

    데이터수신 로직 개선

--- src/session.c
+++ src/session.c
@@ -8,10 +8,28 @@ int do_use_fd (int efd, struct epoll_event ev) {
     int         sizen               = 0; 
     int         readn               = 0; 
 
-
-    char        buf_in[1024]        = { '\0' }; 
+    char        *recv_data                  = NULL;
+    char        buff[MAX_RECV_BUFF_SIZE]    = {'\0'};
     char        readbuf[1024]       = { '\0' }; 
 
+    json_object     *recv_data_json;
+    agent_type_t    agent_type;
+
+    while (1) {
+        recv_data = socket_read(cfd);                       /* 데이터 수신*/
+        recv_data_json = parse_string_to_json(recv_data);   /* 문자열 데이터를 JSON 포멧으로 변환 */
+        agent_type = get_agent_type(recv_data_json); 
+
+        break;
+    }
+
+    do_del_fd(efd,cfd); 
+    close (cfd); 
+    return -1;
+}
+
+
+#if 0
     while(1) {
         DEBUG("");
         readn = read (cfd, readbuf, 255); 
@@ -43,6 +61,9 @@ int do_use_fd (int efd, struct epoll_event ev) {
     do_modify_fd(efd, cfd); 
 #endif // USE_ETONESHOT 
 
+#endif
+
+#if 0
     //memset (buf_in, 0x00, 256); 
     readn = read (cfd, buf_in, 255); 
     // if it occured ploblems with reading, delete from epoll event pool and close socket 
@@ -53,7 +74,28 @@ int do_use_fd (int efd, struct epoll_event ev) {
     } else { 
         printf ("read data %s", buf_in); 
         send (cfd, buf_in, strlen (buf_in), sendflags); 
-    } 
+    }
     return 1; 
 } 
+#endif
+
+char* socket_read(int fd) {
+    int     len = 0;
+
+    char    buff[MAX_RECV_BUFF_SIZE] = {'\0'};
+    char    *recv_data = NULL;
+    
+    recv_data = (char*)malloc(MAX_RECV_BUFF_SIZE);
+
+    while (len == 0) {
+        len += read(fd, buff, MAX_RECV_BUFF_SIZE);
+        if (len < 0) {
+            return recv_data;
+        }
+        realloc(recv_data, len);
+        memcpy(recv_data, buff, len);
+    }
+
+    return recv_data;
+}
 
--- src/session.h
+++ src/session.h
@@ -5,6 +5,9 @@
 #include "json.h"
 #include "json_tokener.h"
 
+#define MAX_RECV_BUFF_SIZE 1024
+
+char* socket_read(int fd);
 int do_use_fd (int efd, struct epoll_event ev);
 
 #endif
