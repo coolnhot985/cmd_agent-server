fed5eeb7c1828e33048c24e5c11431f671e6c5b1
root <root@ewbf.ml>
2018-12-03 17:23:12 +0900

    헤더파일 변경

--- src/main.h
+++ src/main.h
@@ -1,46 +1,45 @@
-#ifndef _TCP_H_
-#define _TCP_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/epoll.h> 
-#include <arpa/inet.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#include <netinet/in.h> 
-#include <netinet/tcp.h> 
-#include <fcntl.h> 
-#include <sys/ioctl.h> 
-#include <errno.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <unistd.h>
 
-#include "cmd_agent.h"
+#define DEBUG(x, a...) printf("%s: " x "\n", __func__, ## a) 
+#define BREAK(x, a...) printf("==## file [%s] func [%s] line [%d] " x "\n", __FILE__, __func__, __LINE__, ## a)
 
-#define PORT        88
+#define PORT                88
+#define EPOLL_SIZE          60
+#define EPOLL_EVENT_SIZE    100
 
-#define EPOLL_SIZE 60 
-#define EPOLL_EVENT_SIZE 100 
+#define AGENT_TYPE_LINUX_NVIDIA     "linux-claymore-nvidia"
+#define AGENT_TYPE_UX               "ux"
 
-int do_use_fd (int efd, struct epoll_event ev);
-int do_accept (int efd, int sfd);
-int do_del_fd(int efd, int cfd);
-int do_add_fd(int efd, int cfd);
-int parse_request_from(char *buf, int buf_len);
+typedef enum {
+    REQ_INVALID         = 0,
+    REQ_UX,
+    REQ_LINUX_CLAYMORE,
+    REQ_WINDOW_CLAYMORE,
+}agent_type_t;
 
-int setnonblock (int sock);
 int settcpnodelay (int sock);
 int setreuseaddr(int sock);
-int get_portnumber_from_arg(int argc, char **argv);
 int init_epoll (int eventsize);
 int init_acceptsock (unsigned short port);
-
-int do_use_fd (int efd, struct epoll_event ev);
 int do_accept (int efd, int sfd);
-int do_add_fd(int efd, int cfd);
 int do_add_fd(int efd, int cfd);
 int do_modify_fd(int efd, int cfd);
 int do_del_fd(int efd, int cfd);
 
-#endif
 
+#endif
--- src/session.h
+++ src/session.h
@@ -1,11 +1,13 @@
 #ifndef _SESSION_H_
 #define _SESSION_H_
 
+#include "tcp.h"
+#include "parse.h"
 #include "main.h"
 #include "json.h"
 #include "json_tokener.h"
 
-#define MAX_RECV_BUFF_SIZE 1024
+#define MAX_RECV_BUFF_SIZE 10
 
 char* socket_read(int fd);
 int do_use_fd (int efd, struct epoll_event ev);
